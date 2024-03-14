896f9a65e8bea7df61a8b3292022e1e0807f8ed7
ZacharyYongBB <zacharyyong_1997@outlook.com>
2024-03-14 17:43:45 +0800

    added FollowerCell, CustomAvatarImage, 3ColumnCollectionView

---
+++ GHFollowers/CustomViews/Cells/FollowerCell.swift
@@ -0,0 +1,12 @@
+//
+//  FollowerCell.swift
+//  GHFollowers
+//
+//  Created by Zachary on 14/3/24.
+//
+
+import UIKit
+
+class FollowerCell: UICollectionViewCell {
+    
+}
---
+++ GHFollowers/CustomViews/CustomAvatarImageView.swift
@@ -0,0 +1,20 @@
+//
+//  CustomAvatarImageView.swift
+//  GHFollowers
+//
+//  Created by Zachary on 14/3/24.
+//
+
+import UIKit
+
+class CustomAvatarImageView: UIImageView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
