127753c2779eb2e8a82b4c9f4bf0f4c76d92534b
ZacharyYongBB <zacharyyong_1997@outlook.com>
2024-03-16 16:06:13 +0800

    added CustomUderInfoHeader UI added download user info added SecondaryTitleLabel added SFSymbols constants

---
+++ GHFollowers/CustomViews/Labels/CustomSecondaryTitleLabel.swift
@@ -0,0 +1,20 @@
+//
+//  CustomSecondaryTitleLabel.swift
+//  GHFollowers
+//
+//  Created by Zachary on 16/3/24.
+//
+
+import UIKit
+
+class CustomSecondaryTitleLabel: UILabel {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ GHFollowers/Utilities/Consts.swift
@@ -0,0 +1,8 @@
+//
+//  const.swift
+//  GHFollowers
+//
+//  Created by Zachary on 16/3/24.
+//
+
+import Foundation
