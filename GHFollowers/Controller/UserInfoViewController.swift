90367679f11e7a28b69c4f92197f65b5bb59e752
ZacharyYongBB <zacharyyong_1997@outlook.com>
2024-03-16 16:48:07 +0800

    added superclass CustomUserInfoViewController refactor UserInfoHeaderView added CustomItemInfoView added SF symbol to consts

--- GHFollowers/Controller/UserInfoViewController.swift
+++ GHFollowers/Controller/UserInfoViewController.swift
@@ -10,16 +10,26 @@ import UIKit
 class UserInfoViewController: UIViewController {
     
     let headerView = UIView()
+    let itemView1 = UIView()
+    let itemView2 = UIView()
+    var itemViews: [UIView] = []
     
     var username: String = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureVC()
+        layoutUI()
+        getUserInfo()
+    }
+    
+    func configureVC() {
         view.backgroundColor = .systemBackground
         let doneButton = UIBarButtonItem(barButtonSystemItem: .close, target: self, action: #selector(dismissVC))
         navigationItem.rightBarButtonItem = doneButton
-        layoutUI()
-        
+    }
+    
+    func getUserInfo() {
         NetworkManager.shared.getUserInfo(username: username) { [weak self] result in
             guard let self = self else { return }
             switch result {
@@ -34,15 +44,33 @@ class UserInfoViewController: UIViewController {
     }
     
     func layoutUI() {
-        view.addSubview(headerView)
+        itemViews = [headerView, itemView1, itemView2]
         
-        headerView.translatesAutoresizingMaskIntoConstraints = false
+        let padding: CGFloat = 20
+        let itemHeight: CGFloat = 140
+        
+        for i in itemViews {
+            view.addSubview(i)
+            i.translatesAutoresizingMaskIntoConstraints = false
+            
+            NSLayoutConstraint.activate([
+                i.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: padding),
+                i.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -padding),
+            ])
+        }
+
+        itemView1.backgroundColor = .systemPink
+        itemView2.backgroundColor = .systemTeal
         
         NSLayoutConstraint.activate([
             headerView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-            headerView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            headerView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            headerView.heightAnchor.constraint(equalToConstant: 180)
+            headerView.heightAnchor.constraint(equalToConstant: 180),
+            
+            itemView1.topAnchor.constraint(equalTo: headerView.bottomAnchor, constant: padding),
+            itemView1.heightAnchor.constraint(equalToConstant: itemHeight),
+            
+            itemView2.topAnchor.constraint(equalTo: itemView1.bottomAnchor, constant: padding),
+            itemView2.heightAnchor.constraint(equalToConstant: itemHeight),
         ])
     }
     
---
+++ GHFollowers/CustomViews/ViewControllers/CustomItemInfoViewController.swift
@@ -0,0 +1,58 @@
+//
+//  CustomItemInfoViewController.swift
+//  GHFollowers
+//
+//  Created by Zachary on 16/3/24.
+//
+
+import UIKit
+
+class CustomItemInfoViewController: UIViewController {
+    
+    let stackView = UIStackView()
+    let itemInfoView1 = CustomItemInfoView()
+    let itemInfoView2 = CustomItemInfoView()
+    let actionButton = CustomButton()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureBackgroundView()
+        layoutUI()
+        configureStackView()
+    }
+    
+    func configureBackgroundView() {
+        view.layer.cornerRadius = 18
+        view.backgroundColor = .secondarySystemBackground
+    }
+    
+    private func configureStackView() {
+        stackView.axis = .horizontal
+        stackView.distribution = .equalSpacing
+        
+        stackView.addArrangedSubview(itemInfoView1)
+        stackView.addArrangedSubview(itemInfoView2)
+    }
+    
+    private func layoutUI() {
+        view.addSubview(stackView)
+        view.addSubview(actionButton)
+        
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        let padding: CGFloat = 20
+        
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: view.topAnchor, constant: padding),
+            stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: padding),
+            stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -padding),
+            stackView.heightAnchor.constraint(equalToConstant: 50),
+            
+            actionButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -padding),
+            actionButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: padding),
+            actionButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -padding),
+            actionButton.heightAnchor.constraint(equalToConstant: 44),
+        ])
+    }
+    
+}
